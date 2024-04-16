a4eb8cd0c714ddba021fc729b0a245350edafa00
hesham hatem <80004117+Hesham942@users.noreply.github.com>
2024-04-16 21:44:43 +0200

    Update Hash Tables.py

--- Hash Tables.py
+++ Hash Tables.py
@@ -4,3 +4,15 @@ for i in range(x):
     d[i]=input()
 for l,v in d.items():
     print(f"{l} : {v}")
+
+print("===========================================================")
+def twosums (nums , target):
+    dict={}
+    for i in range (len(nums)):
+        c=target-nums[i]
+        if c in dict:
+            return [dict[c],i]
+        dict[nums[i]]=i
+    return []
+t=twosums([2,7,11,15],9)
+print(t)
